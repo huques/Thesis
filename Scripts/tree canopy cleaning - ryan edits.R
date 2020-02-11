ec02df5b3496a2b072328f61f0e88373ddf7f8c7
Ryan Kobler <ryankobler@c134h067.wless.reed.edu>
2020-02-10 17:01:04 -0800

    can-o-pies

--- Scripts/tree canopy cleaning - ryan edits.R
+++ Scripts/tree canopy cleaning - ryan edits.R
@@ -1,22 +1,57 @@
 library(sf)
+library(magrittr)
+library(tidyverse)
 
-gdb4 <- "./DATA/tree_canopy.gdb"
-st_layers(gdb4)
-layers <- st_layers(gdb4)
-layers
+# GDB5 was created in ArcMap by intersecting the entire tree_canopy.gdb with the rlis_2019
+# taxlots layer
 
-canopy <- st_read(gdb4, "canopy_class_2014_metro")
-capacity2 <- st_read(gdb4, "bli_capacity_v2")
-bli_constraints2 <- st_read(gdb4, "bli_constraints_v2_pts_run4")
+gdb5 <- "./DATA/canopy_20200210.gdb"
+st_layers(gdb5) # there are two intersection layers in here because i accidentially made a copy
+# they are exactly the same though!
 
-treejoint <- st_join(testjoint4, canopy)
+canopy <- st_read(gdb5, "canopy_taxlot_intersect") # note: this line takes ~4 mins to run
 
-st_geometry(taxlots_pruned)
+# grab the ids we're using in the analysis (from taxlot layer)
+ids <- taxlots_pruned %>%
+  pull(STATE_ID)
 
-lot_geometries <- taxlots_pruned %>%
-  select(STATE_ID)
+# create new data frame in order to calculate total taxlot area for our "percentage of lot 
+# covered by canopy" variable
+taxlot_areas <- taxlots_pruned %>%
+  rename(taxlot_area = Shape_Area) %>%
+  select(STATE_ID, taxlot_area) %>% 
+  st_drop_geometry()
 
-canopy %<>%
-  lwgeom::st_make_valid()
+# This chunk collapses canopy by STATE_ID and generates canopy coverage ratios
+canopy2 <- canopy %>%
+  filter(STATE_ID %in% ids) %>% # filter for just those STATE_IDs in the taxlots pruned data set
+  rename(canopy_area = Shape_Area) %>%
+  left_join(taxlot_areas, by = "STATE_ID") %>% # join the taxlot areas by state_id (for the denominator)
+  st_drop_geometry() %>% # dropped because we had many self-intersections when trying to summarize
+  group_by(STATE_ID) %>% 
+  summarise(pct_canopy_cov = sum(canopy_area) / taxlot_area[1], # take the first element in taxlot_area
+            total_canopy_cov = sum(canopy_area), # save numerator and denominator separately
+            taxlot_area = taxlot_area[1]) 
 
-can_intersect <- st_intersection(lot_geometries[1:10,], canopy)
+# should end up with 33387 observations
+
+# Join the newly created variable to the data frame
+df$pct_canopy_cov <- NA
+
+can_cov_var <- canopy2 %>%
+  select(STATE_ID, pct_canopy_cov)
+
+df <- left_join(df, can_cov_var, by = "STATE_ID")
+
+
+
+  
+
+
+
+
+
+
+
+
+
