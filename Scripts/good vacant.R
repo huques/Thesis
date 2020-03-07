3b479e09c69fb7c6f78a9d1071ca3bdd03227c64
Salma Huque <salmahuque@Salmas-MacBook-Pro.local>
2020-03-07 12:25:55 -0800

    updated script for percent_vacant var

--- Scripts/good vacant.R
+++ Scripts/good vacant.R
@@ -1,41 +1,49 @@
 # Is Vacant Dummy -- Redux
+library(tidyverse)
+library(sf)
+library(mapview)
 
-
-import taxlots 
+#import taxlots 
 
 # explicitly define the missing values in a factor level
 taxlots_copy <- taxlots
 taxlots$STATE_ID <- fct_explicit_na(taxlots$STATE_ID)
 
-# selects property id's and vacancy variable
-vacancy <- taxlots %>%
-  select(STATE_ID, PRPCD_DESC, Shape) %>%
-  rename(ShapeCapacity = Shape)
+# selects variables
+vacancy <- taxlots_copy %>%
+  select(STATE_ID, PRPCD_DESC, Shape)
 
-# check is STATE_ID in taxlots is more than unique
+# check if STATE_ID in taxlots is more than unique -- 161 repeat ids -- it is
 sum(data.frame(table(taxlots$STATE_ID))$Freq > 1)
+# df of repeat obs -- n = 586
 multi_state_id <- taxlots_copy %>%
   group_by(STATE_ID) %>%
   mutate(n = n()) %>%
   filter(n > 1)
 
-
+# df of unique obs -- 197,131 obs
 vacancy_pruned <- vacancy %>%
   group_by(STATE_ID) %>%
   mutate(n = n()) %>%
   filter(n == 1) 
 
+
+# create vacant column
 vacancy_pruned$VACANT <- vacancy_pruned$PRPCD_DESC == "VACANT LAND"
 
 
-# creates a buffer called buffer_dist (measured in meters)
+# creates a buffer called buffer_dist
 # adds this buffer as an additional geometry to the main dataset
-buffer_dist <- 805
+# 1 ft = 0.3048 meters
+conv <- 0.3048
+ft <- 200
+buffer_dist <- ft * conv
 
+# creates buffer of size `buffer_dist` around buffy
 buffy <- taxlots_pruned %>%
   rename(ShapeBuffy = Shape) %>%
   st_buffer(buffer_dist)
-?
+
 # join buffy and vacancy_pruned 
 vacant_join_buffy <- st_join(buffy, vacancy_pruned, left = TRUE) # returns all buffy
 
@@ -44,9 +52,12 @@ vacant_var <- vacant_join_buffy %>%
   arrange(STATE_ID.x) %>%
   group_by(STATE_ID.x) %>%
   mutate(n = n()) %>%
-  mutate(VACANT = recode(VACANT, 
-                         "1" = 1,
-                         "0" = 0)) %>%
+  mutate(VACANT = VACANT*1) %>%
+  filter(!is.na(VACANT)) %>% # taking out na's when calcing percent vacant
   summarize(percent_vacant = sum(VACANT)/n[1])
+
+
+
+
 
 
